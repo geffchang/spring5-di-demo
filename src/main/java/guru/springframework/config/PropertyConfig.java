9b48b4e9fcb28ee17a54ccf370a7c2d5e5408075
geffchang <geffchang@gmail.com>
2019-11-03 18:39:09 +0800

    097 Spring Boot Application.properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,28 +1,15 @@
 package guru.springframework.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
 
 @Configuration()
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-	@PropertySource("classpath:datasource.properties"),
-	@PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-	
-	@Autowired
-	Environment env;
-	
+
 	@Value("${guru.username}")
 	String user;
 	
@@ -44,7 +31,7 @@ public class PropertyConfig {
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource source = new FakeDataSource();
-		source.setUser(env.getProperty("USERNAME"));
+		source.setUser(user);
 		source.setPassword(password);
 		source.setUrl(url);
 		
@@ -53,7 +40,7 @@ public class PropertyConfig {
 	
 	@Bean
 	public FakeJmsBroker fakeJmsBroker() {
-		FakeJmsBroker broker = new FakeJmsBroker();
+		FakeJmsBroker broker = new FakeJmsBroker(); 
 		broker.setUsername(jmsUsername);
 		broker.setPassword(jmsPassword);
 		broker.setUrl(jmsUrl);
@@ -61,9 +48,4 @@ public class PropertyConfig {
 		return broker;
 	}
 
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		return propertySourcesPlaceholderConfigurer;
-	}
 }
